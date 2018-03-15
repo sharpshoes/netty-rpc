e93b782ae654faf275661233408ff18c51ed05b7
Casper <chongyuan.yang@gmail.com>
2018-03-15 23:21:50 +0800

    init

---
+++ src/main/java/org/sharpshoes/netty_rpc/App.java
@@ -0,0 +1,13 @@
+package org.sharpshoes.netty_rpc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
